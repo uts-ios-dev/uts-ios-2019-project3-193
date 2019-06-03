96adbb315cf27337cb4cce232350ddcfe66a6fb3
BlairZx <51222547+BlairZx@users.noreply.github.com>
2019-06-03 01:14:33 -0700

    Update SignInView.swift

--- ass3/SignInView.swift
+++ ass3/SignInView.swift
@@ -3,7 +3,7 @@
 //  ass3
 //
 //  Created by Shirley yamg on 30/5/19.
-//  Copyright © 2019 Vicky Shi. All rights reserved.
+//  Copyright © 2019 Shirley yamg . All rights reserved.
 //
 
 import UIKit
@@ -20,13 +20,19 @@ class SignInView: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // the sign in button function
     @IBAction func register(_ sender: UIButton) {
-
+        // judge username has been inputted
         if regUsername.text!.count > 0{
+            // judge the format of username is valid
             if validateEmail(email: regUsername.text!){
+                // judge password and repassword have been inputted
                 if regPassword.text!.count > 0 && regRePassword.text!.count > 0 {
+                    // checking password and repassword are same
                     if checkPassword(){
+                        // checking username has not been registered
                         if checkLogin(){
+                            // saving username and password
                            UserDefaults.standard.set(regPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines), forKey: regUsername.text!)
                             
                             self.performSegue(withIdentifier: "toMapView", sender: nil)
@@ -48,6 +54,7 @@ class SignInView: UIViewController {
         
     }
 
+    // alert information function
     func alert(messages: String){
         let alertController = UIAlertController(title: "Error", message: messages,
                                                 preferredStyle: .alert)
@@ -56,6 +63,7 @@ class SignInView: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
+    // verify email format
     func validateEmail(email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let emailTest:NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex);
@@ -63,9 +71,13 @@ class SignInView: UIViewController {
         
     }
     
+    // checking password and repassword
     func checkPassword() -> Bool {
+        // get password
         let rpassword = NSString(string:regPassword.text!)
+        // get repassword
         let rrpassword = NSString(string:regRePassword.text!)
+        // compare two password are same
         if rpassword.compare(rrpassword as String).rawValue == 0 {
             return true;
         }else{
@@ -73,6 +85,7 @@ class SignInView: UIViewController {
         }
     }
     
+    // verify username has not been registered
     func checkLogin() -> Bool{
         let userPassword = UserDefaults.standard.string(forKey: regUsername.text!)
 
